b321517cc5cfb396baf199abcf80877ebf7e0dd9
Brian Low <github@brianlow.com>
2022-03-26 18:19:57 -0600

    Sofle Choc variant

---
+++ keyboards/sofle/sofle.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if defined(KEYBOARD_sofle_rev1)
+#    include "rev1.h"
+#elif defined(KEYBOARD_sofle_choc)
+#    include "choc.h"
+#elif defined(KEYBOARD_sofle_keyhive)
+#    include "keyhive.h"
+#endif
