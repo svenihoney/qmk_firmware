f1b6f1033f29aeaaef80864d5fede139e3fcc932
Sven Fischer <sven@leiderfischer.de>
2024-04-25 11:44:19 +0200

    Put ALT key to right side

--- keyboards/sofle_choc/keymaps/sveni/config.h
+++ keyboards/sofle_choc/keymaps/sveni/config.h
@@ -15,10 +15,6 @@
  */
 #pragma once
 
-// Plug USB into the left half of the keyboard
-// see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
-#define MASTER_LEFT
-
 // Enabling this option changes the startup behavior to listen for an
 // active USB communication to delegate which part is master and which
 // is slave. With this option enabled and theres’s USB communication,
@@ -27,75 +23,13 @@
 //
 // I've found this helps with some ProMicros where the slave does not boot
 #define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 5000
+/* #define SPLIT_USB_TIMEOUT 5000 */
 
 #define OLED_TIMEOUT 10000
 #define OLED_FADE_OUT 1
 
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
 
-#ifdef RGB_MATRIX_ENABLE
-
 #define RGB_DISABLE_WHEN_USB_SUSPENDED     // turn off effects when suspended
 #define SPLIT_TRANSPORT_MIRROR             // If LED_MATRIX_KEYPRESSES or LED_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
-#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128  // limits maximum brightness of LEDs (max 255). Higher may cause the controller to crash.
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_BREATHING
-
-// Animations
-// Uncomment any that you want to try. Limited by
-// space on the microcontroller. The defines below
-// mirror the order they animations will cycle through.
-
-// These modes don’t require any additional defines
-#define ENABLE_RGB_MATRIX_ALPHAS_MODS
-// #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-// #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-#define ENABLE_RGB_MATRIX_BREATHING
-// #define ENABLE_RGB_MATRIX_BAND_SAT
-// #define ENABLE_RGB_MATRIX_BAND_VAL
-// #define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-// #define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-// #define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-// #define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-#define ENABLE_RGB_MATRIX_CYCLE_ALL
-// #define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-// #define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-/* #define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON */
-// #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-/* #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL */
-// #define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-// #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-/* #define ENABLE_RGB_MATRIX_DUAL_BEACON */
-// #define ENABLE_RGB_MATRIX_RAINBOW_BEACON
-// #define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-// #define ENABLE_RGB_MATRIX_RAINDROPS
-// #define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-// #define ENABLE_RGB_MATRIX_HUE_BREATHING
-// #define ENABLE_RGB_MATRIX_HUE_PENDULUM
-// #define ENABLE_RGB_MATRIX_HUE_WAVE
-/* #define ENABLE_RGB_MATRIX_PIXEL_FRACTAL */
-// #define ENABLE_RGB_MATRIX_PIXEL_FLOW
-// #define ENABLE_RGB_MATRIX_PIXEL_RAIN
-
-// These modes require the RGB_MATRIX_FRAMEBUFFER_EFFECTS define
-// #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// #define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-// #define ENABLE_RGB_MATRIX_DIGITAL_RAIN
-
-// These modes require the RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES define
-// #define RGB_MATRIX_KEYPRESSES
-// #define RGB_MATRIX_KEYRELEASES
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-// #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-// #define ENABLE_RGB_MATRIX_SPLASH
-// #define ENABLE_RGB_MATRIX_MULTISPLASH
-// #define ENABLE_RGB_MATRIX_SOLID_SPLASH
-// #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-
-#endif
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100  // limits maximum brightness of LEDs (max 255). Higher may cause the controller to crash.
--- keyboards/sofle_choc/keymaps/sveni/keymap.c
+++ keyboards/sofle_choc/keymaps/sveni/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,   KC_MPLY,  KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
-                        KC_LGUI, KC_LALT, KC_LCTL, TL_LOWR, KC_ENT,    KC_SPC,   TL_UPPR,  KC_NUBS,  KC_NUHS, KC_RGUI
+                        KC_LGUI, KC_NUBS, KC_LCTL, TL_LOWR, SC_SENT,   KC_SPC,   TL_UPPR,  KC_NUHS,  KC_LALT, KC_RGUI
     ),
     /* LOWER
     * ,----------------------------------------.                    ,-----------------------------------------.
